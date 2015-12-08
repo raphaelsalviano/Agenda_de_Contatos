47644b93b95fbe189d6c6d5e08f3186b051f9bee
Raphael Salviano <raphael_salviano@outlook.com>
2015-12-08 15:08:12 -0300

    COmmit
    Agenda de Contatos

---
+++ app/src/main/java/br/com/ufpb/ayty/contatos/util/Contato.java
@@ -0,0 +1,104 @@
+package br.com.ufpb.ayty.contatos.util;
+
+
+import java.io.Serializable;
+
+
+public class Contato implements Serializable{
+
+
+    private long id;
+
+    private int idImage;
+
+    private String nome;
+
+    private String sobrenome;
+
+    private String telefone;
+
+    private String tipoTelefone;
+
+    private String email;
+
+    private String tipoEmail;
+
+    public Contato() {
+
+    }
+
+    public Contato(String nome, String sobrenome, String telefone, String email) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public String getNomeCompleto(){
+        return nome + " " + sobrenome;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getIdImage() {
+        return idImage;
+    }
+
+    public void setIdImage(int idImage) {
+        this.idImage = idImage;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getTipoTelefone() {
+        return tipoTelefone;
+    }
+
+    public void setTipoTelefone(String tipoTelefone) {
+        this.tipoTelefone = tipoTelefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTipoEmail() {
+        return tipoEmail;
+    }
+
+    public void setTipoEmail(String tipoEmail) {
+        this.tipoEmail = tipoEmail;
+    }
+}
